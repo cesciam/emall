15297cfb9560e218323257729689a1282a1643cc
sllhero <csc8848@gmail.com>
2020-07-15 20:06:52 -0600

    Commit inicial

---
+++ DataAccessLayer/CRUD/PromocionCrudFactory.cs
@@ -0,0 +1,35 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLayer.CRUD
+{
+    class PromocionCrudFactory : CrudFactory
+    {
+        public override void Create(BaseEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Delete(BaseEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override T Retrieve<T>(BaseEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<T> RetrieveAll<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update(BaseEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Entities/Promocion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities
+{
+    public class Promocion: BaseEntity
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public double porcentaje { get; set; }
+        public string codigo { get; set; }
+        public int cantidad { get; set; }
+        public int id_comercio { get; set; }
+        public int id_sucursal { get; set; }
+
+    }
+}
