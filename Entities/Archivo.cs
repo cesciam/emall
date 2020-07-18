25ea82b56d46897f727565f37b7a8be25af282bc
mikemartinezr7 <xn1p3r@gmail.com>
2020-07-18 14:38:01 -0600

    Actualizado Entities de archivo

--- Entities/Archivo.cs
+++ Entities/Archivo.cs
@@ -1,16 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.Json.Serialization;
 
 namespace Entities {
     public class Archivo : BaseEntity {
+        [JsonPropertyName("id")]
         public int Id { get; set; }
+        [JsonPropertyName("nombre")]
         public string Nombre { get; set; }
+        [JsonPropertyName("enlace")]
         public string Enlace { get; set; }
-        public int Tipo { get; set; }
+        [JsonPropertyName("tipo")]
+        public string Tipo { get; set; }
+        [JsonPropertyName("id_comercio")]
         public int Id_Comercio { get; set; }
 
-        public Archivo() { 
+        public Archivo() {
         }
     }
 }
