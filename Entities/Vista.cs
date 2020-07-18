dfb80059fe84b7cb00ddaeaf1e11e42f701ee16f
jvillalobosm <jvillalobos@ucenfotec.ac.cr>
2020-07-18 10:32:23 -0600

    CRUD-Vista hasta el API

---
+++ Entities/Vista.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities
+{
+    public class Vista : BaseEntity
+    {
+
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public Vista()
+        {
+
+        }
+        public Vista(string[] infoArray)
+        {
+            if (infoArray != null && infoArray.Length >= 2)
+            {
+                var inputId = 0;
+                if (int.TryParse(infoArray[0], out inputId))
+                {
+                    inputId = id;
+                }
+                else
+                {
+                    throw new Exception("La id no es válida");
+                }
+                nombre = infoArray[1];
+            }
+            else
+            {
+                throw new Exception("Todos los valores son requeridos");
+            }
+        }
+
+    }
+}
