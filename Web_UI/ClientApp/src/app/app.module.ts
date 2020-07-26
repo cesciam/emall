e709b058449aade94b868ca19599e119d9abbfdf
mikemartinezr7 <xn1p3r@gmail.com>
2020-07-25 22:32:13 -0600

    Fix para app.module con cambios finales

--- Web_UI/ClientApp/src/app/app.module.ts
+++ Web_UI/ClientApp/src/app/app.module.ts
@@ -1,11 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { NgxMaskModule, IConfig } from 'ngx-mask';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { DashboardAdminComponent } from './dashboard-admin/dashboard-admin.component';
 import { DashboardComercioComponent } from './dashboard-comercio/dashboard-comercio.component';
 import { HeaderComponent } from './homePageComponents/header/header.component';
@@ -15,15 +14,19 @@ import { HeroComponent } from './homePageComponents/hero/hero.component';
 import { FeaturedSpadComponent } from './homePageComponents/featured-spad/featured-spad.component';
 import { BannerComponent } from './homePageComponents/banner/banner.component';
 import { BlogComponent } from './homePageComponents/blog/blog.component';
+import { AgregarUsuarioComponent } from './dashboard-admin/usuario/agregar-usuario/agregar-usuario.component';
+import { ListarUsuarioComponent } from './dashboard-admin/usuario/listar-usuario/listar-usuario.component';
 import { AgregarEmpleadoComponent } from './empleado/agregar-empleado/agregar-empleado.component';
 import { ListarEmpleadoComponent } from './empleado/listar-empleado/listar-empleado.component';
 import { EmpleadoService } from './servicios/empleado.service';
 
+const maskConfig: Partial<IConfig> = {
+  validation: false,
+};
+
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    NavMenuComponent,
     DashboardAdminComponent,
     DashboardComercioComponent,
     HeaderComponent,
@@ -33,19 +36,28 @@ import { EmpleadoService } from './servicios/empleado.service';
     FeaturedSpadComponent,
     BannerComponent,
     BlogComponent,
+    AgregarUsuarioComponent,
+    ListarUsuarioComponent,
     DashboardComercioComponent,
     AgregarEmpleadoComponent,
     ListarEmpleadoComponent,
   ],
   imports: [
+    NgxMaskModule.forRoot(maskConfig),
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'homePage', component: HomePageComponent},
+      { path: '', component: HomePageComponent, pathMatch: 'full' },
       { path: 'agregar-empleado', component: AgregarEmpleadoComponent },
       { path: 'listar-empleado', component: ListarEmpleadoComponent },
+      { path: 'listar-empleado', component: ListarEmpleadoComponent },
+      { path: 'dashboard-admin', component: DashboardAdminComponent },
+      { path: 'dashboard-admin/usuario', component: ListarUsuarioComponent },
+      { path: 'dashboard-admin/usuario/listar-usuario', component: ListarUsuarioComponent },
+      { path: 'dashboard-admin/usuario/agregar-usuario', component: AgregarUsuarioComponent },
+      { path: 'dashboard-comercio', component: DashboardComercioComponent },
     ])
   ],
   providers: [
