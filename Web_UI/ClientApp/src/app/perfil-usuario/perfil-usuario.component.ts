a71e8affa0126f631b33279f310f2a1ba3781d35
Paosa20 <msanchezc1@ucenfotec.ac.cr>
2020-08-04 11:58:37 -0600

    Cambios de html en el landing y los Crud

--- Web_UI/ClientApp/src/app/perfil-usuario/perfil-usuario.component.ts
+++ Web_UI/ClientApp/src/app/perfil-usuario/perfil-usuario.component.ts
@@ -2,6 +2,7 @@ import { ComercioService } from '../services/comercio.service';
 import { Usuario } from '../models/usuario.model';
 import { PerfilAdminComercioComponent } from '../perfil-admin-comercio/perfil-admin-comercio.component';
 import { Component, OnInit } from '@angular/core';
+import { Comercio } from '../models/Comercio';
 
 @Component({
   selector: 'app-perfil-usuario',
@@ -9,14 +10,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./perfil-usuario.component.css']
 })
 export class PerfilUsuarioComponent implements OnInit {
-  private usuarioLogueado: object = null;
+ private usuarioLogueado: object = null;
   private id: number;
+  private comercios: Comercio[];
 
-  constructor(private duenno: PerfilAdminComercioComponent) {
-    this.usuarioLogueado = JSON.parse(localStorage.getItem('usuario-logueado'));
+  constructor(private comercioService: ComercioService) {
+    this.usuarioLogueado= JSON.parse(localStorage.getItem('usuario-logueado'));
     
   }
   ngOnInit() {
-    this.duenno.llenarComercios();
+    let usuarioBuscar: any = JSON.parse(localStorage.getItem('usuario-logueado'));
+    let usuarioActivado: Usuario = usuarioBuscar.usuario;
+
+    this.comerciosDuenno(usuarioActivado);
+  }
+
+  comerciosDuenno(usuario: Usuario) {
+    let comercio: Comercio = new Comercio();
+    comercio.idAdmin = parseInt(usuario.Id);
+    this.comercioService.ObtenerComerciosAdmin(comercio)
+      .subscribe(data => this.comercios = data);
   }
 }
