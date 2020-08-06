549b19adb401417d4b547d4e0196ac9358bfb8d4
mikemartinezr7 <xn1p3r@gmail.com>
2020-08-06 13:50:57 -0600

    Editar perfil de usuario en el sitio

--- Web_UI/ClientApp/src/app/perfil-usuario/perfil-usuario.component.ts
+++ Web_UI/ClientApp/src/app/perfil-usuario/perfil-usuario.component.ts
@@ -10,24 +10,39 @@ import { Comercio } from '../models/Comercio';
   styleUrls: ['./perfil-usuario.component.css']
 })
 export class PerfilUsuarioComponent implements OnInit {
- private usuarioLogueado: object = null;
+ private usuarioLogueado: Usuario = null;
   private id: number;
   private comercios: Comercio[];
+  private section: string = 'info';
+  private image: string;
 
   constructor(private comercioService: ComercioService) {
-    this.usuarioLogueado= JSON.parse(localStorage.getItem('usuario-logueado'));
-    
+    this.usuarioLogueado = JSON.parse(localStorage.getItem('usuario-logueado'));
   }
+
   ngOnInit() {
     let usuarioBuscar: any = JSON.parse(localStorage.getItem('usuario-logueado'));
     let usuarioActivado: Usuario = usuarioBuscar.usuario;
 
+    if (this.usuarioLogueado['usuario'].Foto != null) {
+      this.image = this.usuarioLogueado['usuario'].Foto.enlace;
+    } else {
+      this.image = null;
+    }
+
     this.comerciosDuenno(usuarioActivado);
+  }
+
+  changeSection(section: string): void {
+    this.section = section;
+    window.scroll(0, 0);
   }
 
   comerciosDuenno(usuario: Usuario) {
     let comercio: Comercio = new Comercio();
+
     comercio.idAdmin = parseInt(usuario.Id);
+    
     this.comercioService.ObtenerComerciosAdmin(comercio)
       .subscribe(data => this.comercios = data);
   }
