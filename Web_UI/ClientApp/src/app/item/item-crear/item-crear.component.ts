d6ef9c6a0b6ef552769f884ea647ada27b92cc92
bsequeiraj <bsequeiraj@ucenfotec.ac.cr>
2020-08-09 00:53:32 -0600

    Commit primeras correcciones

--- Web_UI/ClientApp/src/app/item/item-crear/item-crear.component.ts
+++ Web_UI/ClientApp/src/app/item/item-crear/item-crear.component.ts
@@ -2,9 +2,9 @@ import { Component, OnInit, Input, NgZone } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Item } from '../../models/item';
 import { getBaseUrl } from '../../../main';
-//import { Impuesto } from '../../models/impuesto';
 import { ItemService } from '../../services/item.service';
 import { ImpuestoService } from '../../services/impuesto.service';
+import { SucursalService } from '../../services/sucursal.service';
 import { async } from '@angular/core/testing';
 import { FileUploader, FileUploaderOptions, ParsedResponseHeaders } from 'ng2-file-upload';
 import { CloudinaryOptions, CloudinaryUploader } from 'ng2-cloudinary';
@@ -17,6 +17,7 @@ import { Sucursal } from '../../models/Sucursal';
 import { ActivatedRoute } from '@angular/router';
 import { parse } from 'ts-node';
 import { Impuesto } from '../../models/impuesto.model';
+import { Comercio } from '../../models/Comercio';
 
 @Component({
   selector: 'app-item-crear',
@@ -28,13 +29,16 @@ import { Impuesto } from '../../models/impuesto.model';
 
 export class ItemCrearComponent implements OnInit {
 
-  //sucursal: Sucursal;
+  
   sucursal: number;
   message: string;
-  //impuestos: number[];
   impuestos: Impuesto[];
   item: Item;
   error: any;
+  sucursales: Sucursal[];
+  comercio: number;
+  tipo: number;
+  sucursalesSeleccionadas: Array<number>;
 
 
   uploader: CloudinaryUploader;
@@ -42,30 +46,40 @@ export class ItemCrearComponent implements OnInit {
 
 
 
-  constructor(private route: ActivatedRoute, private service: ItemService, private router: Router, private serviceImpuesto: ImpuestoService) {
-    //this.sucursal = JSON.parse(localStorage.getItem('sucursalSeleccionada'))
-    this.sucursal = parseInt(this.route.snapshot.params['id_sucursal']);
-    //this.impuestos = new Impuesto[];
+  constructor(private route: ActivatedRoute, private service: ItemService, private router: Router, private serviceImpuesto: ImpuestoService, private serviceSucursal: SucursalService) {
+    this.comercio = parseInt(this.route.snapshot.params['id_comercio']);
+    this.sucursalesSeleccionadas = new Array<number>();
+
+
+
     this.uploader = new CloudinaryUploader(new CloudinaryOptions({ cloudName: cloudinaryConfig.cloud_name, uploadPreset: cloudinaryConfig.upload_preset }));
     this.foto = 'https://mdbootstrap.com/img/Photos/Others/placeholder.jpg';
     this.item = new Item();
-    //this.sucursal = new Sucursal();
-    //this.sucursal.id = 1;
   }
 
 
-
-
-
-  ngOnInit() {
-    this.sucursal = parseInt(this.route.snapshot.params['id_sucursal']);
-    //this.sucursal = JSON.parse(localStorage.getItem('sucursalSeleccionada'))
+  llenar() {
+    this.serviceSucursal.ObtenerTodoSucursales(this.comercio)
+      .subscribe(data => this.sucursales = data);
     this.serviceImpuesto.ObtenerTodoImpuestoItem().subscribe(
       (data: Impuesto[]) => this.impuestos = data,
       (err: any) => console.log(err)
     );
-    //this.sucursal = new Sucursal();
-    //this.sucursal.id = 1;
+  }
+
+  marcar(e: any, id: number) {
+    console.log(id);
+    if (e.target.checked) {
+      this.sucursalesSeleccionadas.push(id)
+    } else {
+      this.sucursalesSeleccionadas = this.sucursalesSeleccionadas.filter(m => m != id)
+    }
+    console.log(this.sucursalesSeleccionadas);
+  }
+
+
+  ngOnInit() {
+    this.llenar();
 
 
   }
