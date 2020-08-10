fd74687f8a49d52f72377202279389d78a449305
mikemartinezr7 <xn1p3r@gmail.com>
2020-08-10 17:22:05 -0600

    Arregos generales en dashboard de admin

--- Web_UI/ClientApp/src/app/pipes/filter-sucursal.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FilterSucursalPipe } from './filter-sucursal.pipe';
-
-describe('FilterSucursalPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FilterSucursalPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
