9f4c4410a1494fd6a7cf16fb48b8265851db409f
bsequeiraj <bsequeiraj@ucenfotec.ac.cr>
2020-08-10 21:01:23 -0600

    Commit validaciones item completas

--- Web_UI/ClientApp/src/app/item/item-crear/item-crear.component.ts
+++ Web_UI/ClientApp/src/app/item/item-crear/item-crear.component.ts
@@ -39,7 +39,9 @@ export class ItemCrearComponent implements OnInit {
   comercio: number;
   tipo: number;
   sucursalesSeleccionadas: Array<number>;
-
+  inventarios: Array<number>;
+  dura: number;
+  precio: number;
 
 
 
@@ -51,7 +53,9 @@ export class ItemCrearComponent implements OnInit {
   constructor(private route: ActivatedRoute, private service: ItemService, private router: Router, private serviceImpuesto: ImpuestoService, private serviceSucursal: SucursalService) {
     this.comercio = parseInt(this.route.snapshot.params['id_comercio']);
     this.sucursalesSeleccionadas = new Array<number>();
-
+    this.inventarios = new Array<number>();
+    this.dura = 0;
+    this.precio = 0;
 
 
     this.uploader = new CloudinaryUploader(new CloudinaryOptions({ cloudName: cloudinaryConfig.cloud_name, uploadPreset: cloudinaryConfig.upload_preset }));
@@ -86,7 +90,64 @@ export class ItemCrearComponent implements OnInit {
       this.sucursalesSeleccionadas.push(id)
     } else {
       this.sucursalesSeleccionadas = this.sucursalesSeleccionadas.filter(m => m != id)
+      let tmp_num2 = this.inventarios.indexOf(id)
+      var tmp_inventario = (document.getElementById("inventario" + id)) as HTMLInputElement;
+      let numero = Number(tmp_inventario.value);
+      this.inventarios.splice(tmp_num2, 1)
+      this.inventarios.splice(tmp_num2, 1)
+      console.log(this.inventarios)
     }
+  }
+
+  inventarioadd(e: any, id: number) {
+    
+    
+    var tmp_inventario = (document.getElementById("inventario" + id)) as HTMLInputElement;
+    let numero = Number(tmp_inventario.value);
+    console.log(numero)
+    console.log(id)
+
+    let tmp_num2 = this.inventarios.indexOf(id)
+
+    console.log(tmp_num2)
+
+    if (tmp_num2 != -1 && numero > 0) {
+      this.inventarios[tmp_num2 + 1] = numero;
+      console.log(this.inventarios)
+    } else if (tmp_num2 == -1 && numero > 0) {
+      this.inventarios.push(id)
+      this.inventarios.push(numero)
+      console.log(this.inventarios)
+    } else if (tmp_num2 != -1 && numero <= 0) {
+      //let tmp_num = this.inventarios.indexOf(id)
+      this.inventarios.splice(tmp_num2, 1)
+      this.inventarios.splice(tmp_num2, 1)
+      console.log(this.inventarios)
+      //if (tmp_num > -1) {
+      //  this.inventarios.splice(tmp_num, 1)
+      //  console.log(this.inventarios)
+      //}
+    }
+
+
+
+    
+  }
+
+  duracionadd(e: any) {
+
+
+    var tmp_duracion = (document.getElementById("duracion")) as HTMLInputElement;
+    this.dura = Number(tmp_duracion.value);
+    console.log(this.dura)
+  }
+
+  precioadd(e: any) {
+
+
+    var tmp_precio = (document.getElementById("precio")) as HTMLInputElement;
+    this.precio = Number(tmp_precio.value);
+    console.log(this.precio)
   }
 
 
@@ -105,6 +166,7 @@ export class ItemCrearComponent implements OnInit {
         this.item.inventario = Number(tmp_inventario.value);
         this.item.id_sucursal = this.sucursalesSeleccionadas[i];
         this.item.id_foto = this.foto;
+        this.item.precio = this.precio;
         var e = (document.getElementById("id_impuesto")) as HTMLSelectElement;
         var sel = e.selectedIndex;
 
@@ -130,7 +192,8 @@ export class ItemCrearComponent implements OnInit {
 
     } else if (this.item.tipo == 'Servicio') {
       for (let i = 0; i < this.sucursalesSeleccionadas.length; i++) {
-        
+        this.item.precio = this.precio;
+        this.item.duracion = this.dura;
         this.item.inventario = 0;
         this.item.id_sucursal = this.sucursalesSeleccionadas[i];
         this.item.id_foto = this.foto;
