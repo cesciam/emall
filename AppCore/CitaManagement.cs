c1fe9190cc4e799866c02e929ecc7726ac44461b
sllhero <csc8848@gmail.com>
2020-08-12 00:35:20 -0600

    Adelanto de validaciones

---
+++ AppCore/CitaManagement.cs
@@ -0,0 +1,46 @@
+﻿using DataAccessLayer.CRUD;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppCore
+{
+    public class CitaManagement
+    {
+        private CitaCrudFactory crud;
+        private CitaManagement()
+        {
+            crud = new CitaCrudFactory();
+        }
+
+        public void Create(BaseEntity entity)
+        {
+
+
+
+            crud.Create(entity);
+        }
+
+
+        public Cita Retrieve(BaseEntity entity)
+        {
+            return crud.Retrieve<Cita>(entity);
+        }
+
+        public List<Configuracion> RetrieveAll()
+        {
+            return crud.RetrieveAll<Configuracion>();
+        }
+
+        public void Delete(BaseEntity entity)
+        {
+            crud.Delete(entity);
+        }
+
+        public void Update(BaseEntity entity)
+        {
+            crud.Update(entity);
+        }
+    }
+}
